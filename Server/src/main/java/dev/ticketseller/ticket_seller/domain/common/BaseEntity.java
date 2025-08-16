93aa0e629c8c2939df12a7dde326d4113e309792
DHKim7762 <149556729+ehdgusdl@users.noreply.github.com>
2025-08-17 01:57:16 +0900

    생성 수정시간 추가ㅣ

--- Server/src/main/java/dev/ticketseller/ticket_seller/domain/common/BaseEntity.java
+++ Server/src/main/java/dev/ticketseller/ticket_seller/domain/common/BaseEntity.java
@@ -1,18 +1,25 @@
 package dev.ticketseller.ticket_seller.domain.common;
 
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-
 import java.time.LocalDateTime;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @CreatedDate
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
-    @LastModifiedBy
-    private LocalDateTime updateAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
-    private boolean is_deleted;
 }
